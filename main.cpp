a44878341d8a05312e9685ab217fbf51c3d6ec79
AlganOngun <alganunlu1896@gmail.com>
2022-10-17 20:48:47 +0300

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include <fplus/fplus.hpp>
+#include <iostream>
+#include <string>
+#include <vector>
+
+auto pop(const auto container) {
+  auto temp_container {container};
+  temp_container.pop_back();
+  return temp_container;
+}
+
+auto plus_operator(const std::vector<int>& container) {
+  return fplus::fwd::apply(
+      container,
+      fplus::fwd::reverse(),
+      fplus::fwd::tail(),
+      fplus::fwd::tail(),
+      fplus::fwd::reverse(),
+      fplus::fwd::append_elem(container[container.size() - 2] + fplus::last(container)));
+}
+
+auto minus_operator(const std::vector<int>& container) {
+  return fplus::fwd::apply(
+      container,
+      fplus::fwd::reverse(),
+      fplus::fwd::tail(),
+      fplus::fwd::tail(),
+      fplus::fwd::reverse(),
+      fplus::fwd::append_elem(container[container.size() - 2] - fplus::last(container)));
+}
+
+auto multiply_operator(const std::vector<int>& container) {
+  return fplus::fwd::apply(
+      container,
+      fplus::fwd::reverse(),
+      fplus::fwd::tail(),
+      fplus::fwd::tail(),
+      fplus::fwd::reverse(),
+      fplus::fwd::append_elem(container[container.size() - 2] * fplus::last(container)));
+}
+
+auto division_operator(const std::vector<int>& container) {
+  return fplus::fwd::apply(
+      container,
+      fplus::fwd::reverse(),
+      fplus::fwd::tail(),
+      fplus::fwd::tail(),
+      fplus::fwd::reverse(),
+      fplus::fwd::append_elem(container[container.size() - 2] / fplus::last(container)));
+}
+
+auto parse(const std::vector<int>&& acc, const std::string& x) -> std::vector<int> {
+  if (x == "+") return plus_operator(acc);
+  else if (x == "-") return minus_operator(acc);
+  else if (x == "*") return multiply_operator(acc);
+  else if (x == "/") return division_operator(acc);
+  else if (std::stoi(x)) return fplus::append(acc, {std::stoi(x)});
+  return {0};
+}
+
+int main() {
+  const auto input {std::string("8 2 / 5 2 * +")};
+  const auto tokens {fplus::split_by_token(std::string(" "), false, input)};
+  const auto result {fplus::last(fplus::fold_left(parse, std::vector<int> {}, tokens))};
+
+  std::cout << result << std::endl;
+}
